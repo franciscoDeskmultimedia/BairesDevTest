e5348d1c91bef0b1857710b5184ab8087342f46d
francisco <francisco.deskmultimedia@gmail.com>
2018-04-07 00:07:02 -0500

    Page for each album showing last two images

--- main.js
+++ main.js
@@ -5,18 +5,18 @@ $(document).ready(function() {
         success:function(data){
 
           var albunes = data;
+          console.log(data[data.length-1]);
           var PrimerAlbum = [];
           var SegundoAlbum = [];
           var TercerAlbum = [];
-         $mostRecentFirst =data.pop().albumId;
-         $mostRecentSecond =data.pop().albumId-1;
-         $mostRecentThird =data.pop().albumId-2;
+         $mostRecentFirst =data[data.length-1].albumId;
+         $mostRecentSecond =data[data.length-1].albumId-1;
+         $mostRecentThird =data[data.length-1].albumId-2;
          var contadorUno = 0;
          var contadorDos = 0;
          var contadorTres = 0;
 
          for(var i in data){
-          console.log(data[i].albumId);
           if(data[i].albumId == $mostRecentFirst){
             //console.log(albunes[i].title);
             //console.log(contadorUno)
@@ -42,56 +42,40 @@ $(document).ready(function() {
        };
        $(".album:nth-child(1) h1").text(PrimerAlbum[0].title);
        $(".album:nth-child(1) .album-image img").attr("src",PrimerAlbum[0].thumbnailUrl);
+       $(".album:nth-child(1)").attr("data-album",PrimerAlbum[0].albumId);
 
        $(".album:nth-child(2) h1").text(SegundoAlbum[0].title);
        $(".album:nth-child(2) .album-image img").attr("src",SegundoAlbum[0].thumbnailUrl);
+       $(".album:nth-child(2)").attr("data-album",SegundoAlbum[0].albumId);
 
        $(".album:nth-child(3) h1").text(TercerAlbum[0].title);
        $(".album:nth-child(3) .album-image img").attr("src",TercerAlbum[0].thumbnailUrl);
+       $(".album:nth-child(3)").attr("data-album",TercerAlbum[0].albumId);
 
-       $(".album a").click(function(event){
+       $(".album").click(function(event){
          event.preventDefault();
-         console.log("lola");
-       })
-     }//.then(function(data) {
-    //     var albunes = data;
-    //     var PrimerAlbum = [];
-    //     var SegundoAlbum = [];
-    //     var TercerAlbum = [];
-    //    $mostRecentFirst =Number(data.pop().albumId);
-    //    $mostRecentSecond =data.pop().albumId-1;
-    //    $mostRecentThird =data.pop().albumId-2;
-    //    var contadorUno = 0;
-    //    var contadorDos = 0;
-    //    var contadorTres = 0;
-    //
-    //    for(var i= 0 ; i<=albunes.length; i++){
-    //     //console.log("FOR :"+albunes[i].albumId);
-    //     if(albunes[i].albumId == $mostRecentFirst){
-    //       //console.log(albunes[i].title);
-    //       //console.log(contadorUno)
-    //       PrimerAlbum.push({"albumId": albunes[i].albumId,"id": albunes[i].id,"title": albunes[i].title,"url": albunes[i].url,"thumbnailUrl": albunes[i].thumbnailUrl});
-    //       localStorage.setItem("PrimerAlbum"+contadorUno,JSON.stringify({"albumId": albunes[i].albumId,"id": albunes[i].id,"title": albunes[i].title,"url": albunes[i].url,"thumbnailUrl": albunes[i].thumbnailUrl}));
-    //       //console.log(PrimerAlbum[contadorUno])
-    //       contadorUno = contadorUno + 1;
-    //     };
-    //     if(albunes[i].albumId ==  $mostRecentSecond){
-    //       SegundoAlbum.push({"albumId": albunes[i].albumId,"id": albunes[i].id,"title": albunes[i].title,"url": albunes[i].url,"thumbnailUrl": albunes[i].thumbnailUrl});
-    //       localStorage.setItem("SegundoAlbum"+contadorDos,JSON.stringify({"albumId": albunes[i].albumId,"id": albunes[i].id,"title": albunes[i].title,"url": albunes[i].url,"thumbnailUrl": albunes[i].thumbnailUrl}));
-    //       //console.log(SegundoAlbum[contadorDos])
-    //       contadorDos = contadorDos + 1;
-    //     };
-    //     if(albunes[i].albumId ==  $mostRecentThird){
-    //       TercerAlbum.push({"albumId": albunes[i].albumId,"id": albunes[i].id,"title": albunes[i].title,"url": albunes[i].url,"thumbnailUrl": albunes[i].thumbnailUrl});
-    //       localStorage.setItem("TercerAlbum"+contadorTres,JSON.stringify({"albumId": albunes[i].albumId,"id": albunes[i].id,"title": albunes[i].title,"url": albunes[i].url,"thumbnailUrl": albunes[i].thumbnailUrl}));
-    //       //console.log(TercerAlbum[contadorTres])
-    //       contadorTres = contadorTres + 1;
-    //     };
-    //     //jQuery(".album:nth-child(1) .id-title h1").text(PrimerAlbum[1].title)
-    //    };
-    //
-    // });
-
-
-});
+         $(".render-last-albums").slideUp("slow");
+         $(".SelectedAlbum").slideDown("slow");
+         $getAtribute = $(this).attr("data-album");
+         if($getAtribute == PrimerAlbum[0].albumId){
+           $(".selectedAlbumPhotos li:first-child img").attr("src",PrimerAlbum[PrimerAlbum.length-1].thumbnailUrl);
+           $(".selectedAlbumPhotos li:last-child img").attr("src",PrimerAlbum[PrimerAlbum.length-2].thumbnailUrl);
+         }
+         if($getAtribute == SegundoAlbum[0].albumId){
+           console.log("segundo");
+           $(".selectedAlbumPhotos li:first-child img").attr("src",SegundoAlbum[SegundoAlbum.length-1].thumbnailUrl);
+           $(".selectedAlbumPhotos li:last-child img").attr("src",SegundoAlbum[SegundoAlbum.length-2].thumbnailUrl);
+         }
+         if($getAtribute == TercerAlbum[0].albumId){
+           console.log("tercero");
+           $(".selectedAlbumPhotos li:first-child img").attr("src",TercerAlbum[TercerAlbum.length-1].thumbnailUrl);
+           $(".selectedAlbumPhotos li:last-child img").attr("src",TercerAlbum[TercerAlbum.length-2].thumbnailUrl);
+         }
+       });
+       $(".BackButton").click(function(){
+         $(".render-last-albums").slideDown("slow");
+         $(".SelectedAlbum").slideUp("slow");
+       });
+     }
+   });
 });
